9fe32acae0d3901900f017d8c9dd401b7cdf90f2
Oleh <turyansky.o.v@gmail.com>
2022-05-31 18:23:09 +0300

    Добавьте файлы проекта.

---
+++ LengthSide/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Создать классы Point и Figure.
+
+Класс Point должен содержать два целочисленных поля и одно строковое поле.
+
+Создать три свойства с одним методом доступа get.
+
+Создать пользовательский конструктор, в теле которого проинициализируйте поля значениями
+аргументов. 
+
+Класс Figure должен содержать конструкторы, которые принимают от 3-х до 5-ти
+аргументов типа Point.
+
+Создать два метода: double LengthSide(Point A, Point B), который рассчитывает длину
+стороны многоугольника; void PerimeterCalculator(), который рассчитывает периметр
+многоугольника.
+
+Написать программу, которая выводит на экран название и периметр многоугольника.
+*/
+namespace LengthSide
+{
+    class Point
+    {
+        private  int coordinX;
+        private  int coordinY;
+        private  string pointName;
+        public int CoordinX
+        {
+            get { return coordinX; }
+        }
+        public int CoordinY
+        {
+            get { return coordinY; }
+        }
+        public string PointName
+        {
+            get { return pointName; }
+        }
+        public Point(int coordinX, int coordinY, string pointName)
+        {
+            this.coordinX = coordinX;
+            this.coordinY = coordinY;
+            this.pointName = pointName;
+        }              
+    }
+    
+    class Figure
+    {   // запрашиваем количество вершин фигуры ? запускаем нужный конструктор - формируем фигуру
+        public Figure(Point A, Point B,Point C)
+        {
+            // triangle
+        }
+        public Figure (Point A,Point B, Point C, Point D)
+        {
+            // quadrilateral
+        }
+        public Figure(Point A, Point B, Point C, Point D, Point E)
+        {
+            // pentagon
+        }
+
+        double length;
+
+
+
+        public double LengthSide(Point A, Point B)
+        {
+            // определяем длины сторон (логика, перебираем пары точек, в зависимости от того какая фигура образована ?)
+            return length;
+        }
+        public static void PerimeterCalculator()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0, i = 0;  string name=null;
+            Console.WriteLine("=> FIGURE: 1 - triangle;  2 - quadrilateral;   3 - pentagon");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(new string ('_', 50));
+            Point point1 = new Point(x, y, name);
+            Point point2 = new Point(x, y, name);
+            Point point3 = new Point(x, y, name);
+            Point point4 = new Point(x, y, name);
+            Point point5 = new Point(x, y, name);
+            switch (i)
+            {
+                case 1:                    
+                    Console.WriteLine("       FIGURE: triangle");
+                    for (int j = 1; j<4; j++)
+                    {
+                        Console.WriteLine(" " + j + "-я точка: ");
+                        if (j == 1)
+                        {                            
+                            name = "A";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());                            
+                            point1 = new Point(x, y, name);
+                        }
+                        if (j == 2)
+                        {
+                            name = "B";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point2 = new Point(x, y, name);
+                        }
+                        if (j == 3)
+                        {
+                            name = "C";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point3 = new Point(x, y, name);
+                        }
+                    }
+                    Figure figure1 = new Figure(point1, point2, point3);  
+                    
+                    break;
+                case 2:
+                    Console.WriteLine("       FIGURE: quadrilateral");
+                    for (int j = 1; j < 5; j++)
+                    {
+                        Console.WriteLine(" " + j + "-я точка: ");
+                        if (j == 1)
+                        {
+                            name = "A";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point1 = new Point(x, y, name);
+                        }
+                        if (j == 2)
+                        {
+                            name = "B";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point2 = new Point(x, y, name);
+                        }
+                        if (j == 3)
+                        {
+                            name = "C";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point3 = new Point(x, y, name);
+                        }
+                        if (j == 4)
+                        {
+                            name = "D";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point4 = new Point(x, y, name);
+                        }
+                    }
+                    Figure figure2 = new Figure(point1, point2, point3, point4);
+                    break;                
+                case 3:
+                    Console.WriteLine("       FIGURE: pentagon");
+                    for (int j = 1; j < 6; j++)
+                    {
+                        Console.WriteLine(" " + j + "-я точка: ");
+                        if (j == 1)
+                        {
+                            name = "A";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point1 = new Point(x, y, name);
+                        }
+                        if (j == 2)
+                        {
+                            name = "B";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point2 = new Point(x, y, name);
+                        }
+                        if (j == 3)
+                        {
+                            name = "C";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point3 = new Point(x, y, name);
+                        }
+                        if (j == 4)
+                        {
+                            name = "D";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point4 = new Point(x, y, name);
+                        }
+                        if (j == 5)
+                        {
+                            name = "E";
+                            x = Convert.ToInt32(Console.ReadLine());
+                            y = Convert.ToInt32(Console.ReadLine());
+                            point4 = new Point(x, y, name);
+                        }
+                    }
+                    Figure figure3 = new Figure(point1, point2, point3, point4,point5);
+                    break;
+                default:
+                    //код, выполняемый если выражение не имеет ни одно из выше указанных значений
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ LengthSide/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LengthSide")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LengthSide")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c38a53c6-6b5b-4109-b6e8-0c395981adb2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
